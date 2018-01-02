f04d89fa7495511aa768636414e007aed487bce2
ctedie <ctedie@centumadeneo.com>
2018-01-02 17:00:56 +0100

    [master]  * Modification du module fifo

--- master/DRIVER_LIB/fifo_lib.c
+++ master/DRIVER_LIB/fifo_lib.c
@@ -1,128 +1,53 @@
-////////////////////////////////////////////////////////////////////////////////
-/// \addtogroup FIFO
-/// @{
-///
-/// \par Justification of design choices
-///     (If required)
-///     - Any important choice shall be justify. For instance:
-///         - Reason why using or not a mutex? Which variables are protected.
-///         - Use of specific driver. 
-///         - Use of specific algorithm.
-///         - ...
-///
-/// \file 
-/// \brief Implementation file of module FIFO
-///
-///
-////////////////////////////////////////////////////////////////////////////////
+/**********************************************************************
+ * COPYRIGHT (C) Alstom 2017 - AC4 project
+ * All rights reserved.
+ **********************************************************************/
 
-/// \cond IGNORE_FOLLOWING
-// For the management of the declaration AND definition of exported variables
-#define FIFO_M
-/// \endcond
+/******************** FILE HEADER *************************************/
+/** \addtogroup FIFO
+ *  \{ */
 
-////////////////////////////////////////////////////////////////////////////////
-//                                                                      Includes
-////////////////////////////////////////////////////////////////////////////////
-// First standard includes (compilation tool / Adetel Group standard / Librairies)
+/**
+ **********************************************************************
+ * \file fifo.c
+ * \brief  FIFO implementation
+ *
+ **********************************************************************/
 
-// Then project includes
-#include "fifo_lib.h"
+/** \} */
 
-////////////////////////////////////////////////////////////////////////////////
-//                                                      Internal Defines & Types
-////////////////////////////////////////////////////////////////////////////////
+/**********************************************************************
+ * INCLUDES
+ **********************************************************************/
+#include <stddef.h>
+#include "fifo.h"
+#include "it_utils.h"
+
+/** \addtogroup SCI
+ *  \{ */
+
+/**********************************************************************
+ * MACROS DEFINITION
+ **********************************************************************/
 
 
-////////////////////////////////////////////////////////////////////////////////
-//                                                            Internal Variables
-////////////////////////////////////////////////////////////////////////////////
-// Note : static is MANDATORY
+/**********************************************************************
+ *  TYPE DEFINITION
+ **********************************************************************/
 
 
-////////////////////////////////////////////////////////////////////////////////
-//                                                            Internal Functions
-////////////////////////////////////////////////////////////////////////////////
+
+/**********************************************************************
+ *  LOCAL VARIABLES OF MODULE
+ **********************************************************************/
 
 
-////////////////////////////////////////////////////////////////////////////////
-//                                                            Exported Functions
-////////////////////////////////////////////////////////////////////////////////
+/**********************************************************************
+ *  LOCAL FUNCTIONS
+ **********************************************************************/
 
-////////////////////////////////////////////////////////////////////////////////
-/// \par Implementation details
-///  Details of implementation
-///  Specific par can be added for certified project (Coverage, Pseudo-code,...)
-////////////////////////////////////////////////////////////////////////////////
-void FIFO_Init(struct FIFO_t *pFifo)
+static inline void appendElem(struct FIFO_t *pFifo, struct FIFO_Elem_t *pElem)
 {
-    CHECK_ASSERTION(NULL != pFifo, return);
-
-    OS_CRITSECT_CREATE(pFifo->hCritSect);
-    pFifo->pHead = NULL;
-    pFifo->pTail = NULL;
-    pFifo->pSelf = pFifo;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-/// \par Implementation details
-/// Remind: the array passed un parameter shall be an array of structures and the first member
-///         of theses structures shall be a FIFO_Elem_t
-///         the array is passed as an array of uint8_t to view it as a buffer, elemSize and arrayDim
-///         parameters allow to calculate the address of each element of the array and to iterate over
-///         the array
-///  Details of implementation
-///  Specific par can be added for certified project (Coverage, Pseudo-code,...)
-////////////////////////////////////////////////////////////////////////////////
-void FIFO_InitFromArray(struct FIFO_t *pFifo, uint8_t array[], uint32_t elemSize, uint32_t arrayDim)
-{
-    uint32_t i;
-    uint8_t *pArrayElem;
-    struct FIFO_Elem_t *pElem;
-
-    CHECK_ASSERTION(NULL != pFifo, return);
-
-    // initialize the FIFO
-    FIFO_Init(pFifo);
-
-    // fill it with the elements of the array
-    pArrayElem = &array[0];
-    for (i = 0LU ; i < arrayDim ; ++i)
-    {
-        // VIOLATION REPORTED: MISRA-C:2012 R.11.1,R.11.2,R.11.3,R.11.5,R.11.7: Casting operation to a pointer
-        //                     MISRA-C:2012 R.11.1,R.11.2,R.11.3,R.11.7: Casting operation on a pointer
-        //                     MISRA-C:2012 R.11.2,R.11.3: Cast to an unrelated type
-        // JUSTIFICATION: Violation of MISRA-C:2012 R.11.3 is acceptable as the address of each elem in the
-        //                array is calculated with the elemSize passed in parameter
-        //LDRA_INSPECTED 94 S
-        //LDRA_INSPECTED 95 S
-        //LDRA_INSPECTED 554 S
-        pElem = (struct FIFO_Elem_t *)pArrayElem;
-        // indicate that the elem is no more in a FIFO (for the check in the insert)
-        pElem->pNext = NULL;
-        FIFO_Insert(pFifo, pElem);
-        // next elem in the array
-        pArrayElem += elemSize;
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-/// \par Implementation details
-///  Details of implementation
-///  Specific par can be added for certified project (Coverage, Pseudo-code,...)
-////////////////////////////////////////////////////////////////////////////////
-void FIFO_Insert(struct FIFO_t *pFifo, struct FIFO_Elem_t *pElem)
-{
-    DECLARE_CRITSECT_LOCVAR;
-
-    // Entry in a critical section
-    OS_CRITSECT_ENTER(pFifo->hCritSect);
-
-    CHECK_ASSERTION(NULL != pFifo, return);
-    CHECK_ASSERTION(pFifo == pFifo->pSelf, return);
-    // check that the element is not in a FIFO
-    CHECK_ASSERTION(NULL == pElem->pNext, return);
-
     if (pFifo->pHead == NULL)
     {
         // FIFO is empty
@@ -138,34 +63,120 @@ void FIFO_Insert(struct FIFO_t *pFifo, struct FIFO_Elem_t *pElem)
 
     // Update tail
     pFifo->pTail = pElem;
-
-    // Exit the critical section
-    OS_CRITSECT_LEAVE(pFifo->hCritSect);
 }
 
-////////////////////////////////////////////////////////////////////////////////
-/// \par Implementation details
-///  Details of implementation
-///  Specific par can be added for certified project (Coverage, Pseudo-code,...)
-////////////////////////////////////////////////////////////////////////////////
-struct FIFO_Elem_t *FIFO_Get(struct FIFO_t *pFifo)
+/**********************************************************************
+ *  API FUNCTIONS
+ **********************************************************************/
+
+/**
+ **********************************************************************
+ * \brief  Initialize a FIFO structure
+ *
+ * \param [in] pFifo   pointer on the FIFO structure
+ *
+ * \note This function is not thread/IT safe
+ **********************************************************************/
+void FIFO_Init(struct FIFO_t *pFifo)
 {
-    struct FIFO_Elem_t *pRetElem;
-    DECLARE_CRITSECT_LOCVAR;
+    if (pFifo != NULL)
+    {
+        pFifo->pHead = NULL;
+        pFifo->pTail = NULL;
+    }
+}
+
+/**
+ **********************************************************************
+ * \brief  Initialize a FIFO structure and fill it with the elements of the array
+ *         in parameter
+ *
+ * \param [in] pFifo    pointer on the FIFO structure
+ * \param [in] array    pointer on the array (view as a buffer of int8u)
+ * \param [in] elemSize size of an element of this array
+ * \param [in] arrayDim number of elements of this array
+ *
+ * \note The array shall be an array of structures which first elem shall be a FIFO_Elem_t
+ *
+ * \note This function is not thread/IT safe
+ **********************************************************************/
+void FIFO_InitFromArray(struct FIFO_t *pFifo, void *array, uint32_t elemSize, uint32_t arrayDim)
+{
+    uint32_t i;
+
+    if (pFifo == NULL || array == NULL)
+    {
+        return;
+    }
+
+    // initialize the FIFO
+    FIFO_Init(pFifo);
+
+    // fill it with the elements of the array
+    for (i = 0u; i < arrayDim; ++i)
+    {
+        appendElem(pFifo, array);
+
+        // next elem in the array
+        array = ((uint8_t*)array) + elemSize;
+    }
+}
+
+/**
+ **********************************************************************
+ * \brief  Insert an element at the tail of the FIFO (the function is thread/IT safe)
+ *
+ * \param [in] pFifo    pointer on the FIFO structure
+ * \param [in] pElem    pointer on the element to insert in the FIFO
+ *
+ * \note The function return if:
+ *       - The fifo passed in parameter is not valid (NULL pointer or invalid structure)
+ *       - The fifo elem passed in parameter is not valid (NULL pointer or invalid structure)
+ **********************************************************************/
+void FIFO_Insert(struct FIFO_t *pFifo, struct FIFO_Elem_t *pElem)
+{
+    ItCpuCtx_t itCtx;
+
+    if (pFifo == NULL || pElem == NULL)
+    {
+        return;
+    }
 
     // Entry in a critical section
-    OS_CRITSECT_ENTER(pFifo->hCritSect);
+    itCtx = IT_SaveDisable();
 
-    CHECK_ASSERTION(NULL != pFifo, return NULL);
-    CHECK_ASSERTION(pFifo == pFifo->pSelf, return NULL);
+    appendElem(pFifo, pElem);
 
-    if (pFifo->pHead == NULL)
+    // Exit the critical section
+    IT_Restore(itCtx);
+}
+
+/**
+ **********************************************************************
+ * \brief  Retrieve and remove an element at the head of the FIFO (the function is thread/IT safe)
+ *
+ * \param [in] pFifo    pointer on the FIFO structure
+ *
+ * \note The function raises an error if:
+ *       - The fifo passed in parameter is not valid (NULL pointer or invalid structure)
+ *
+ * \return pointer on the element removed from the head of the FIFO
+ *         (NULL if the FIFO was empty)
+ **********************************************************************/
+struct FIFO_Elem_t *FIFO_Get(struct FIFO_t *pFifo)
+{
+    struct FIFO_Elem_t *pRetElem = NULL;
+    ItCpuCtx_t itCtx;
+
+    if (pFifo == NULL)
     {
-        // FIFO is empty
-        // a NULL pointer is returned
-        pRetElem = NULL;
+        return NULL;
     }
-    else
+
+    // Entry in a critical section
+    itCtx = IT_SaveDisable();
+
+    if (pFifo->pHead != NULL)
     {
         // FIFO not empty
         // the element at the head of the FIFO is returned
@@ -181,52 +192,56 @@ struct FIFO_Elem_t *FIFO_Get(struct FIFO_t *pFifo)
         // indicate that the elem is no more in a FIFO (for the check in the insert)
         pRetElem->pNext = NULL;
     }
+    else
+    {
+        // FIFO is empty
+        // a NULL pointer is returned
+    }
 
     // Exit the critical section
-    OS_CRITSECT_LEAVE(pFifo->hCritSect);
+    IT_Restore(itCtx);
 
-    // VIOLATION REPORTED: MISRA-C:2012 R.18.6: Pointer assignment to wider scope
-    // JUSTIFICATION: No violation of MISRA-C:2012 R.18.6: pRetElem do not point on an object with automatic storage
-    //LDRA_INSPECTED 71 S
     return pRetElem;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-/// \par Implementation details
-///  Details of implementation
-///  Specific par can be added for certified project (Coverage, Pseudo-code,...)
-////////////////////////////////////////////////////////////////////////////////
-bool FIFO_IsEmpty(const struct FIFO_t *pFifo)
+/**
+ **********************************************************************
+ * \brief  Append a Fifo pFifo to a destination Fifo pFifo_dest
+ *
+ * \param [in] pFifo_dest    pointer on the destination FIFO structure
+ * \param [in] pFifo    pointer on the FIFO structure
+ *
+ * \note  pFifo should not be used again after
+ **********************************************************************/
+void FIFO_AppendFifo(struct FIFO_t *pFifoDest, struct FIFO_t *pFifo)
 {
-    CHECK_ASSERTION(NULL != pFifo, return false);
-    CHECK_ASSERTION(pFifo == pFifo->pSelf, return false);
+    ItCpuCtx_t itCtx;
 
-    return NULL == pFifo->pHead ? true : false;
+    if (pFifo == NULL)
+    {
+        return;
+    }
+
+    // Entry in a critical section
+    itCtx = IT_SaveDisable();
+
+    if (pFifoDest->pHead == NULL)
+    {
+        // FIFO is empty
+        *pFifoDest = *pFifo;
+    }
+    else
+    {
+        // FIFO not empty
+        pFifoDest->pTail->pNext = pFifo->pHead;
+
+        // Update tail
+        pFifoDest->pTail = pFifo->pTail;
+    }
+
+    // Exit the critical section
+    IT_Restore(itCtx);
 }
-////////////////////////////////////////////////////////////////////////////////
-/// \par Implementation details
-///  Details of implementation
-///  Specific par can be added for certified project (Coverage, Pseudo-code,...)
-////////////////////////////////////////////////////////////////////////////////
-struct FIFO_Elem_t *FIFO_ReadHead(const struct FIFO_t *pFifo)
-{
-    CHECK_ASSERTION(NULL != pFifo, return NULL);
-    CHECK_ASSERTION(pFifo == pFifo->pSelf, return NULL);
 
-    return pFifo->pHead;
-}
-////////////////////////////////////////////////////////////////////////////////
-/// \par Implementation details
-///  Details of implementation
-///  Specific par can be added for certified project (Coverage, Pseudo-code,...)
-////////////////////////////////////////////////////////////////////////////////
-struct FIFO_Elem_t *FIFO_ReadNext(const struct FIFO_Elem_t *pElem)
-{
-    CHECK_ASSERTION(NULL != pElem, return NULL);
-
-    return pElem->pNext;
-}
-
-///
-/// @}
-///
+/** \} */
+/* EOF */
