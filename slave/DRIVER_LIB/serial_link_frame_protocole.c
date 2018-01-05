d36024d57bab10a57b8647ebb6ada41dcc70a001
ctedie <ctedie@centumadeneo.com>
2018-01-05 18:35:54 +0100

    [slave]  * Mise en place du protocole de trames

--- slave/DRIVER_LIB/serial_link_frame_protocole.c
+++ slave/DRIVER_LIB/serial_link_frame_protocole.c
@@ -31,15 +31,33 @@
 #include "driverlib/uart.h"
 
 #include "serial_link.h"
+#include "serial_link_frame_protocol.h"
 
 /////////////////////////////////////////////////////////////////////////////////
 // Private typedef
 /////////////////////////////////////////////////////////////////////////////////
+///
+typedef void (*cbCharTreatment_t)(struct rx_data *pDataRx, uint8_t car);
+
+typedef struct rx_data
+{
+    cbCharTreatment_t charTreatment;
+
+    uint8_t *pMsg;
+    uint8_t *receivedChar;
+    uint16_t msgSize;
+
+    cbNotifyRx_t cbNotifyRx;
+    void* pDataNotifyRX;
+
+}rx_data_t;
 
 /////////////////////////////////////////////////////////////////////////////////
 // Private define
 /////////////////////////////////////////////////////////////////////////////////
-
+#define STX             0x02
+#define ETX             0x03
+#define DLE             0x10
 /////////////////////////////////////////////////////////////////////////////////
 // Private macro
 /////////////////////////////////////////////////////////////////////////////////
@@ -55,8 +73,12 @@
 /////////////////////////////////////////////////////////////////////////////////
 // Private function prototypes
 /////////////////////////////////////////////////////////////////////////////////
+static void charReceived(void *pDataRx, uint8_t car);
 
-
+static void waitStartDLE(rx_data_t *pDataRx, uint8_t car);
+static void waitSTX(rx_data_t *pDataRx, uint8_t car);
+static void waitDataWithoutDLE(rx_data_t *pDataRx, uint8_t car);
+static void waitDataWithDLE(rx_data_t *pDataRx, uint8_t car);
 /////////////////////////////////////////////////////////////////////////////////
 // Functions
 /////////////////////////////////////////////////////////////////////////////////
@@ -72,6 +94,143 @@
 bool SerialLinkFrameProtocoleInit(void)
 {
 	return false;
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+// Local Functions
+/////////////////////////////////////////////////////////////////////////////////
+///
+/////////////////////////////////////////////////////////////////////////////////
+/// \brief
+///
+/// \param
+///
+/// \return
+///
+/////////////////////////////////////////////////////////////////////////////////
+static void charReceived(void *pData, uint8_t car)
+{
+    rx_data_t* pDataRx = &pData;
+    pDataRx->charTreatment(pDataRx, car);
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+/// \brief
+///
+/// \param
+///
+/// \return
+///
+/////////////////////////////////////////////////////////////////////////////////
+static void waitStartDLE(rx_data_t *pDataRx, uint8_t car)
+{
+    if(car == DLE)
+    {
+        pDataRx->charTreatment = (cbCharTreatment_t)waitSTX;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+/// \brief
+///
+/// \param
+///
+/// \return
+///
+/////////////////////////////////////////////////////////////////////////////////
+static void waitSTX(rx_data_t *pDataRx, uint8_t car)
+{
+    if(car == STX)
+    {
+        if(pDataRx->pMsg == NULL) //No previous buffer so try to alloc one
+        {
+            //TODO Alloc pMsg
+        }
+
+        if(pDataRx->pMsg)
+        {
+            //Initialise the message informations
+            pDataRx->msgSize = 0;
+            pDataRx->receivedChar =  pDataRx->pMsg;
+            pDataRx->charTreatment = (cbCharTreatment_t)waitDataWithoutDLE;
+        }
+        else //No available buffer
+        {
+            pDataRx->charTreatment = (cbCharTreatment_t)waitStartDLE;
+        }
+    }
+    else
+    {
+        pDataRx->charTreatment = (cbCharTreatment_t)waitStartDLE;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+/// \brief
+///
+/// \param
+///
+/// \return
+///
+/////////////////////////////////////////////////////////////////////////////////
+static void waitDataWithoutDLE(rx_data_t *pDataRx, uint8_t car)
+{
+    if(car == DLE)
+    {
+        pDataRx->charTreatment = (cbCharTreatment_t)waitDataWithDLE;
+    }
+    else
+    {
+        *pDataRx->receivedChar = car;
+        pDataRx->receivedChar++;
+        pDataRx->msgSize++;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+/// \brief
+///
+/// \param
+///
+/// \return
+///
+/////////////////////////////////////////////////////////////////////////////////
+static void waitDataWithDLE(rx_data_t *pDataRx, uint8_t car)
+{
+
+    pDataRx->charTreatment = (cbCharTreatment_t)waitDataWithoutDLE;
+
+    if(car == DLE)
+    {
+        //DLE double Stocker 1 DLE en tant que data
+        *pDataRx->receivedChar = car;
+        pDataRx->msgSize++;
+    }
+    else if(car == STX)
+    {
+        // Nouvelle trame : Abandon de la precedente et début d'une nouvelle
+        pDataRx->msgSize = 0;
+        pDataRx->receivedChar = pDataRx->pMsg;
+    }
+    else if(car == ETX)
+    {
+        // Fin de la trame : Notifier la couche supérieure
+        pDataRx->charTreatment = (cbCharTreatment_t)waitStartDLE;
+        pDataRx->cbNotifyRx(pDataRx->pDataNotifyRX, pDataRx->pMsg, pDataRx->msgSize);
+
+        //Liberer la memoire msg
+        pDataRx->pMsg = NULL;
+    }
+    else
+    {
+        //Erreur : Abandonner la trame
+
+        pDataRx->charTreatment = (cbCharTreatment_t)waitStartDLE;
+
+        //Liberer la memoire msg
+        pDataRx->pMsg = NULL;
+    }
+
 }
 
 ///
